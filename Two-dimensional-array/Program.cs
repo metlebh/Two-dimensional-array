10c1d6e73ef5d6cae45cc56c9b20595cb3f2049c
Matlab Y. Huseynov <matlabyh@code.local>
2023-01-18 18:22:03 +0400

    1-ni yaza bildim

---
+++ Two-dimensional-array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Two_dimensional_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[,] arr = new int[3, 3]
+                {
+                      {1,2,3},
+                      {4,5,6},
+                      {7,8,9},
+                };
+            int sum = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        sum += arr[i, j];
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
